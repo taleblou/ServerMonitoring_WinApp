a1c57c358d12c26573a8d769c43cd59b5d40e72f
Morteza Taleblou <morteza_taleblou@yahoo.com>
2025-01-16 14:15:57 +0200

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace ServerMonitor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.URL_TextBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.URL_TextBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.URL_TextBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.URL_TextBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.URL_TextBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
+            this.URL_TextBox6 = new System.Windows.Forms.TextBox();
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.Time_TextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Alert_CheckBox = new System.Windows.Forms.CheckBox();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL :";
+            // 
+            // URL_TextBox1
+            // 
+            this.URL_TextBox1.Location = new System.Drawing.Point(91, 49);
+            this.URL_TextBox1.Name = "URL_TextBox1";
+            this.URL_TextBox1.Size = new System.Drawing.Size(398, 26);
+            this.URL_TextBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(507, 52);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(139, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(507, 95);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(139, 23);
+            this.progressBar2.TabIndex = 5;
+            // 
+            // URL_TextBox2
+            // 
+            this.URL_TextBox2.Location = new System.Drawing.Point(91, 92);
+            this.URL_TextBox2.Name = "URL_TextBox2";
+            this.URL_TextBox2.Size = new System.Drawing.Size(398, 26);
+            this.URL_TextBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "URL :";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(507, 142);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(139, 23);
+            this.progressBar3.TabIndex = 8;
+            // 
+            // URL_TextBox3
+            // 
+            this.URL_TextBox3.Location = new System.Drawing.Point(91, 139);
+            this.URL_TextBox3.Name = "URL_TextBox3";
+            this.URL_TextBox3.Size = new System.Drawing.Size(398, 26);
+            this.URL_TextBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "URL :";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(507, 188);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(139, 23);
+            this.progressBar4.TabIndex = 11;
+            // 
+            // URL_TextBox4
+            // 
+            this.URL_TextBox4.Location = new System.Drawing.Point(91, 185);
+            this.URL_TextBox4.Name = "URL_TextBox4";
+            this.URL_TextBox4.Size = new System.Drawing.Size(398, 26);
+            this.URL_TextBox4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "URL :";
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(507, 234);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(139, 23);
+            this.progressBar5.TabIndex = 14;
+            // 
+            // URL_TextBox5
+            // 
+            this.URL_TextBox5.Location = new System.Drawing.Point(91, 231);
+            this.URL_TextBox5.Name = "URL_TextBox5";
+            this.URL_TextBox5.Size = new System.Drawing.Size(398, 26);
+            this.URL_TextBox5.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "URL :";
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.Location = new System.Drawing.Point(507, 283);
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(139, 23);
+            this.progressBar6.TabIndex = 17;
+            // 
+            // URL_TextBox6
+            // 
+            this.URL_TextBox6.Location = new System.Drawing.Point(91, 280);
+            this.URL_TextBox6.Name = "URL_TextBox6";
+            this.URL_TextBox6.Size = new System.Drawing.Size(398, 26);
+            this.URL_TextBox6.TabIndex = 16;
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.Location = new System.Drawing.Point(59, 444);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(566, 31);
+            this.Start_Button.TabIndex = 18;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = true;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // Time_TextBox
+            // 
+            this.Time_TextBox.Location = new System.Drawing.Point(91, 330);
+            this.Time_TextBox.Name = "Time_TextBox";
+            this.Time_TextBox.Size = new System.Drawing.Size(54, 26);
+            this.Time_TextBox.TabIndex = 20;
+            this.Time_TextBox.Text = "30";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 337);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Time :";
+            // 
+            // Alert_CheckBox
+            // 
+            this.Alert_CheckBox.AutoSize = true;
+            this.Alert_CheckBox.Location = new System.Drawing.Point(38, 383);
+            this.Alert_CheckBox.Name = "Alert_CheckBox";
+            this.Alert_CheckBox.Size = new System.Drawing.Size(101, 24);
+            this.Alert_CheckBox.TabIndex = 22;
+            this.Alert_CheckBox.Text = "Play Alert";
+            this.Alert_CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(38, 506);
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(608, 26);
+            this.textBoxLog.TabIndex = 23;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 561);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.Alert_CheckBox);
+            this.Controls.Add(this.Time_TextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Start_Button);
+            this.Controls.Add(this.progressBar6);
+            this.Controls.Add(this.URL_TextBox6);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.URL_TextBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.URL_TextBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.URL_TextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.URL_TextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.URL_TextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Server Check";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox URL_TextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox URL_TextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.TextBox URL_TextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.TextBox URL_TextBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.TextBox URL_TextBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar6;
+        private System.Windows.Forms.TextBox URL_TextBox6;
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.TextBox Time_TextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox Alert_CheckBox;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerMonitor
+{
+    public partial class Main : Form
+    {
+        private bool isMonitoring = false;
+        private int time = 30;
+        private string csvFile = "url_reports.csv";
+        private readonly TextBox[] urlTextBoxes = new TextBox[10];
+        private readonly ProgressBar[] progressBar = new ProgressBar[10];
+
+        public Main()
+        {
+            InitializeComponent();
+            urlTextBoxes[0] = URL_TextBox1;
+            urlTextBoxes[1] = URL_TextBox2;
+            urlTextBoxes[2] = URL_TextBox3;
+            urlTextBoxes[3] = URL_TextBox4;
+            urlTextBoxes[4] = URL_TextBox5;
+            urlTextBoxes[5] = URL_TextBox6;
+
+
+
+
+            progressBar[0] = progressBar1;
+            progressBar[1] = progressBar2;
+            progressBar[2] = progressBar3;
+            progressBar[3] = progressBar4;
+            progressBar[4] = progressBar5;
+            progressBar[5] = progressBar6;
+        }
+
+        private async void Start_Button_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(Time_TextBox.Text.Trim(), out time))
+            {
+                if (time < 5)
+                {
+                    time = 5;
+                }
+            }
+            else
+            {
+                // Handle invalid input (you can set a default value or display an error message)
+                time = 5; // Default value
+            }
+            if (isMonitoring)
+            {
+                isMonitoring = false;
+                Start_Button.Text = "Start Monitoring";
+                return;
+            }
+
+            isMonitoring = true;
+            Start_Button.Text = "Stop Monitoring";
+
+            bool writeHeaders = !File.Exists(csvFile);
+            string directory = Path.GetDirectoryName(csvFile);
+             
+
+            while (isMonitoring)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    string url = urlTextBoxes[i].Text.Trim();
+                    if (string.IsNullOrEmpty(url))
+                    {
+                        continue; // Skip empty URLs
+                    }
+                    try
+                    {
+                        var pingTime = await PingHostAsync(new Uri(url).Host);
+                        var (loadingTime, statusCode, responseSize, errorMessage) = await CheckUrlAsync(url);
+                       
+
+                        var currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                        var data = $"{currentTime},{pingTime},{loadingTime},{responseSize},{statusCode},{errorMessage}";
+
+                        using (var sw = new StreamWriter(csvFile, true))
+                        {
+                            if (writeHeaders)
+                            {
+                                sw.WriteLine("Date,Ping Time (ms),Loading Time (s),Response Size (bytes),Status Code,Error Message");
+                                writeHeaders = false;
+                            }
+                            sw.WriteLine(data);
+                        }
+                        if (statusCode == 200)
+                        {
+                            progressBar[i].BackColor = Color.Green;
+                            progressBar[i].Maximum = 100;
+                            progressBar[i].Minimum = 0;
+                            progressBar[i].Value = (int)NormalizeLoadingTime(pingTime);
+                        }
+                        else
+                        {
+                            if (Alert_CheckBox.Checked)
+                            {
+                                PlaySound();
+                            }
+                            progressBar[i].BackColor = Color.Red;
+                        }
+                        textBoxLog.AppendText($"{currentTime} - Ping: {pingTime} ms, Load: {loadingTime} s, Size: {responseSize} bytes, Status: {statusCode}, Error: {errorMessage}{Environment.NewLine}");
+                    }
+                    catch (Exception ex)
+                    {
+                        textBoxLog.AppendText($"Error: {ex.Message}{Environment.NewLine}");
+                    }
+                }
+
+                await Task.Delay(time* 1000); // Wait for 30 seconds
+            }
+        }
+
+        private async Task<long> PingHostAsync(string hostNameOrAddress)
+        {
+            try
+            {
+                using (var ping = new Ping())
+                {
+                    var reply = await ping.SendPingAsync(hostNameOrAddress);
+                    return reply.RoundtripTime;
+                }
+            }
+            catch (PingException ex)
+            {
+                // Handle ping exception
+                textBoxLog.AppendText($"Ping Error: {ex.Message}{Environment.NewLine}");
+                return -1; // Return an invalid ping time
+            }
+        }
+
+        private async Task<(double loadingTime, int? statusCode, long? responseSize, string errorMessage)> CheckUrlAsync(string url)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url);
+                    var contentLength = response.Content.Headers.ContentLength;
+                    var statusCode = (int)response.StatusCode;
+                    stopwatch.Stop();
+
+                    return (stopwatch.Elapsed.TotalSeconds, statusCode, contentLength, string.Empty);
+                }
+            }
+            catch (Exception ex)
+            {
+                stopwatch.Stop();
+                return (stopwatch.Elapsed.TotalSeconds, null, null, ex.Message);
+            }
+        }
+        private double NormalizeLoadingTime(double loadingTime)
+        {
+            // Update min and max values
+            double minLoadingTime = Math.Min(0, loadingTime);
+            double maxLoadingTime = Math.Max(100, loadingTime);
+
+            // Avoid division by zero if minLoadingTime == maxLoadingTime
+            if (minLoadingTime == maxLoadingTime)
+            {
+                return 100; // In case of no variation, set it to 100
+            }
+
+            // Normalize the loading time to be between 0 and 100
+            double normalizedLoadingTime = ((loadingTime - minLoadingTime) / (maxLoadingTime - minLoadingTime)) * 100;
+
+            return Math.Min(100, Math.Max(0, normalizedLoadingTime)); // Ensure it's between 0 and 100
+        }
+        public void PlaySound()
+        {
+            SoundPlayer player = new SoundPlayer("path_to_sound_file.wav");
+            player.Play();  
+        }
+    }
+}
